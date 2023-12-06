2f896ba7740d8a6ea6a02fc414e8349c920b898a
风吟 <3160257358@qq.com>
2023-12-06 09:59:13 +0800

    12.6

--- 代码/洛谷exe/二分查找与二分搜索/烦恼的高考志愿P1678.cpp
+++ 代码/洛谷exe/二分查找与二分搜索/烦恼的高考志愿P1678.cpp
@@ -1,50 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 int m,n,school[100005],grades[1000005];
-int degree=0;
+long long ans=0;
 int  main()
 {
     cin>>m>>n;//m个学校，n个学生
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
         cin>>school[i];
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin>>grades[i];
     }
-    sort(school,school+m);
-    sort(grades,grades+n);
-    for (int i = 0; i < n; i++)
+    sort(school+1,school+m+1);
+    //sort(grades,grades+n);
+    for (int i = 1; i <= n; i++)
     {
-        int L=0,R=m,mid,ans;
+        int L=0,R=m+1,mid;
         //mid=L+R>>1;
-        ans=grades[i]-school[0];//第一个不满意度
-        while(L<=R)
+        //ans=abs(grades[i]-school[0]);//第一个不满意度
+        while(L<R)
         {
-            mid=L+R>>1;
-            if(grades[i]-school[mid]<ans)
+            mid=(L+R)/2;  
+            if(school[mid]<=grades[i])
             {
-                ans=mid;
-                R=mid-1;
-            }
-            else{
+                //ans=abs(grades[i]-school[mid]);
+                ////pixot=mid;
                 L=mid+1;
             }
+            else{
+                R=mid;
+                //L++;
+            }
         }
-        degree+=grades[i]-school[mid];    }
-}
-int Find(int L,int R)//使用前确保答案在[L,R]内
-{
-    int ans,mid;
-    while(L<=R)//闭区间上的二分条件
-    {
-        int mid=L+R>>1;
-        if(P(mid))//条件成立
-            ans=mid;R=mid-1;
-            //只需要记录满足条件的mid，最后循环一定会结束，也一定会在ans当中保留正确的答案
+        if(grades[i]<=school[1])
+            ans+=school[1]-grades[i];
         else 
-            L=mid+1;//L和R不用考虑+1和-1，全部写上去
+            ans+=min(abs(school[L-1]-grades[i]),abs(school[L]-grades[i]));
+        //degree+=abs(ans);    
     }
-    return ans;
+    cout<<ans;
 }
+// int Find(int L,int R)//使用前确保答案在[L,R]内
+// {
+//     int ans,mid;
+//     while(L<=R)//闭区间上的二分条件
+//     {
+//         int mid=L+R>>1;
+//         if(P(mid))//条件成立
+//             ans=mid;R=mid-1;
+//             //只需要记录满足条件的mid，最后循环一定会结束，也一定会在ans当中保留正确的答案
+//         else 
+//             L=mid+1;//L和R不用考虑+1和-1，全部写上去
+//     }
+//     return ans;
+// }
