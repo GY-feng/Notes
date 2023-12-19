03bf7f46760d6796b6a36a1c8bd4698552a34f39
风吟 <3160257358@qq.com>
2023-12-19 20:28:01 +0800

    12.19

--- 代码/ABD2023算法比赛/B单调栈.cpp
+++ 代码/ABD2023算法比赛/B单调栈.cpp
@@ -1,47 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
 using namespace std;
-#define MAX_N 1e5 + 3
-int n;
-int f[100003];
-stack<int> st;
+
+// ProblemB
 int main()
 {
+    int n = 0;
     cin >> n;
+    int numbers[100005],res[100005];
     for (int i = 0; i < n; i++)
     {
-        cin >> f[i];
+        cin >> numbers[i];
     }
-    st.push(f[0]);
-    cout << "-1"
-         << " ";
-    for (int i = 1; i < n; i++)
+    stack<int> s;
+    for (int i = 0; i < n; i++)
     {
-            if (!st.empty() && st.top() < f[i]) // 栈不为空或者即将入栈的元素大于栈第一个
-            {
-                cout << st.top() << " ";
-                st.push(f[i]); // 元素入栈
-            }
-            else
-            {
-                vector<int> aaa;
-                aaa.push_back(f[i]);
-                while (!st.empty() && st.top() > f[i])
-                {
-                    aaa.push_back(st.top());
-                    st.pop();
-                }
-                if(st.empty())//栈全空了，说明都不满足
-                {
-                    cout<<"-1"<<" ";
-                }
-                else{
-                    cout<<st.top();
-                }
-                for(int i=aaa.size();i>=0;i--)
-                {
-                    st.push(aaa[i]);//加回去栈里面
-                }
-            }
+        while (!s.empty() && numbers[i] <= s.top()) // 栈不为空并且遍历到的元素比栈顶（比遍历到的数还要先入栈）还小
+        {
+            s.pop();
+        }
+        s.empty() ? res[i]=-1:res[i]= s.top();
+        s.push(numbers[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+            cout<<res[i]<<' ';
     }
     cin>>n;
 }
