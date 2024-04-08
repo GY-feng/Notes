bf459b592e2c8bb81860f735e6a71a3ca57d81d0
风吟 <3160257358@qq.com>
2024-04-08 20:45:04 +0800

    4.8

--- 代码/蓝桥/23省赛A/2-分糖果.py
+++ 代码/蓝桥/23省赛A/2-分糖果.py
@@ -1,4 +1,15 @@
 import os
 import sys
-a="caabdc"
-print(sorted(a))
+
+def dic(str1,str2):
+    len_min=min(len(str1),len(str2))
+    #逐个字符比较直到找到不同的地方：
+    for i in range(len_min):
+        if str1[i]!=str2[i]:
+            return ord(str1[i])-ord(str2[i])
+    #如果前面的字符都是相同的话，那么长度大的字符串字典序更大
+    return len(str1)-len(str2)
+
+n,x=map(int,input().split())
+a=input()
+a=sorted(a)
