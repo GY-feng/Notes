07b0e51de5026a646cb305b5d4289d4b7a0646a2
风吟 <3160257358@qq.com>
2024-04-09 01:28:48 +0800

    4.9

--- 代码/蓝桥/23省赛A/2-分糖果.py
+++ 代码/蓝桥/23省赛A/2-分糖果.py
@@ -1,15 +1,18 @@
 import os
 import sys
-
-def dic(str1,str2):
-    len_min=min(len(str1),len(str2))
-    #逐个字符比较直到找到不同的地方：
-    for i in range(len_min):
-        if str1[i]!=str2[i]:
-            return ord(str1[i])-ord(str2[i])
-    #如果前面的字符都是相同的话，那么长度大的字符串字典序更大
-    return len(str1)-len(str2)
-
 n,x=map(int,input().split())
-a=input()
-a=sorted(a)
+s=input()
+s=sorted(s)
+
+if s[x-1]!=s[0]:# 分配给最后一人的糖果价值>第一人的价值
+    print(s[x-1])
+    pass
+else:# 每个人拿到的开始的相同
+    if s[x]==s[-1]:# 后面剩余的糖果一样
+        print(s[x-1],end='')
+        for i in range(x,n,x):
+            print(s[i],end='')
+    else:
+        for i in s[x-1:]:# s的第x个元素开始，迭代到列表末尾
+            print(i,end='') # end=''输出完i后不换行
+
