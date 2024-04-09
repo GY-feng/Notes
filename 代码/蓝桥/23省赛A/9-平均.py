cd8af773665b002d4d7189d0d797d54f34c06c58
风吟 <3160257358@qq.com>
2024-04-09 20:05:31 +0800

    4.9

--- 代码/蓝桥/23省赛A/9-平均.py
+++ 代码/蓝桥/23省赛A/9-平均.py
@@ -1,35 +1,27 @@
 import os
 import sys
-
-class Num:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
+from collections import defaultdict
 
 n=int(input())
-l=[]
-def count(j):
-    b_max=0
-    for h in l:
-        if h.a!=j:
-            b_max+=h.b
-    return b_max
-
+time=n//10 # 每个数字需要出现的次数
+# l=[[]]*10# 如果您希望创建一个包含 10 个独立的空列表的列表，您应该避免使用乘法操作符 *，因为它会创建包含对相同空列表对象的引用的列表。
+l=[[]for i in range(10)]
+#print("time:",time)
 for i in range(n):
-    a,b=(input().split())
-    a=int(a)
-    b=int(b)
-    x=Num(a,b)
-    l.append(x)
+    a,b=map(int,input().split())
+    l[a].append(b)
+    pass
 
-b_max=count(0)
-print(b_max)
-for i in range(1,10):
-    c=count(i)
-    print(c)
-    if c<b_max:
-        b_max=c
-print(c)
+
+# 列表 l 将会按照每个子列表的第一个元素 a 的升序进行排序。如果有多个子列表的第一个元素相同，则会按照第二个元素 b 的升序进行排序。
+s=0
+for i in range(10):
+    ls=sorted(l[i])
+    s+=sum(ls[:-time])
+# 除了最后 time 个元素之外的所有元素的和添加到总和 s
+print(s)
+
+
 
 
 
