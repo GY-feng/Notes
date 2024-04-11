71b2889019fcd611f41ff758506fcd2de8e827a6
风吟 <3160257358@qq.com>
2024-04-11 16:23:44 +0800

    4.11

---
+++ 算法-py内置库/数学2.py
@@ -0,0 +1,14 @@
+#判断一个数是否是质数：
+def iszhi(n):
+    if n<=1:
+        return 0
+    elif n<=3:
+        return 1
+    elif n%2==0 or n%3==0:
+        return 0
+    i=5
+    while i*i<=n:
+        if n%i==0 or n%(i+2)==0:
+            return 0
+        i+=6
+    return 1
